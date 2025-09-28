f0d825bd10a4ecc6fed285ce90219fa96769d0a2
khw7385 <khw7385@naver.com>
2025-09-28 15:50:11 +0900

    feat: 이력서 서비스 url 수정

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -7,7 +7,7 @@ export interface ResumeInfo {
 
 export const getResumeInfo = async (): Promise<ResumeInfo | undefined> => {
     try {
-        const response = await axios.get("https://raw.githubusercontent.com/khw7385/first-deploy/refs/heads/feat/07-resume/service/resume-info.json");
+        const response = await axios.get("https://raw.githubusercontent.com/khw7385/first-deploy/refs/heads/main/service/resume-info.json");
 
         console.log(response.data);
         return response.data;
